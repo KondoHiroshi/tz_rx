ed129ac6092c8c04723c58476fff0bd1dd6ea120
ogawahideo <ogawahideo@MacBook-Air.local>
2018-12-06 16:40:59 +0900

    tz

--- scripts/cpz3177.py
+++ scripts/cpz3177.py
@@ -24,14 +24,13 @@ if __name__ == '__main__':
 
     sis_pub_list = [rospy.Publisher('{0}_rsw{1}_diff{2}'.format(node_name, rsw_id, ch), Float64, queue_size=1)
                        for ch in sis_ch_list]
-"""
+
     pm_pub_list = [rospy.Publisher('{0}_rsw{1}_diff{2}'.format(node_name, rsw_id, ch), Float64, queue_size=1)
                        for ch in pm_ch_list]
 """
-
     pub1=rospy.Publisher('{0}_rsw{1}_diff10'.format(node_name, rsw_id), Float64, queue_size=1)
     pub2=rospy.Publisher('{0}_rsw{1}_diff11'.format(node_name, rsw_id), Float64, queue_size=1)
-
+"""
     try:
         ad = pyinterface.open(3177, rsw_id)
     except OSError as e:
@@ -39,14 +38,13 @@ if __name__ == '__main__':
         sys.exit()
 
 while not rospy.is_shutdown():
-"""
+
     for ch, pub in zip(pm_ch_list, pm_pub_list):
         ret = ad.input_voltage(ch, "diff")
         msg = Float64()
         msg.data = ret
         pub.publish(msg)
 """
-
     ret = ad.input_voltage(10, "diff")
     msg = Float64()
     msg.data = ret
@@ -56,6 +54,7 @@ while not rospy.is_shutdown():
     msg = Float64()
     msg.data = ret
     pub2.publish(msg)
+"""
 
     for ch, pub in zip(sis_ch_list, sis_pub_list):
         ret = ad.input_voltage(ch, "diff")
