1bd92306172cc4aec25764b0cf641703c83a9cd8
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2019-06-12 17:37:03 +0900

    add

--- scripts/test.py
+++ scripts/test.py
@@ -16,21 +16,32 @@ class test(object):
 
         self.pub1 = rospy.Publisher("/pub1", Int32, queue_size=1)
         self.pub2 = rospy.Publisher("/pub2", Int32, queue_size=1)
+        self.pub3 = rospy.Publisher("/pub3", Int32, queue_size=1)
+        self.pub4 = rospy.Publisher("/pub4", Int32, queue_size=1)
         self.pub3 = rospy.Publisher("/test", Int32, queue_size=1)
         self.sub1 = rospy.Subscriber("/sub1", Int32, self.test1)
         self.sub2 = rospy.Subscriber("/sub2", Int32, self.test2)
 
 #switch
     def test1(self,q):
-        a = q.data*2
+        a = q.data*1
         self.pub1.publish(a)
         return
 
     def test2(self,q):
-        a = q.data*3
+        a = q.data*2
         self.pub2.publish(a)
         return
 
+    def test3(self,q):
+        a = q.data*3
+        self.pub1.publish(a)
+        return
+
+    def test4(self,q):
+        a = q.data*4
+        self.pub2.publish(a)
+        return
 
 if __name__ == "__main__" :
     rospy.init_node("test")
