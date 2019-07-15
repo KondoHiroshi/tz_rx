644c8571d623058ef17097b2e9950328b9e57d0c
ogawalab <ogawalab@ogawalab-no-MacBook-Air.local>
2019-07-15 11:55:45 +0900

    test

--- scripts/test.py
+++ scripts/test.py
@@ -78,29 +78,10 @@ class make_pub(object):
         time.sleep(0.1)
         return
 
-class make_pub_test(object):
-
-    def __init__(self):
-        self.pub = {}
-        pass
-
-    def publish(self, topic_name, data_class, msg):
-        if topic_name not in self.pub:
-            self.set_publisher(topic_name = topic_name, data_class = data_class)
-            pass
-
-        self.pub[topic_name].publish(msg)
-        return
-
-    def set_publisher(self, topic_name, data_class):
-        self.pub[topic_name] = rospy.Publisher(name = topic_name, data_class = data_class, queue_size = 1, latch = True)
-        time.sleep(0.1)
-        return
-
 class test(object):
 
     def __init__(self):
-        self.make_pub = make_pub_test()
+        self.make_pub = make_pub()
 
     def test_f(self, data):
         topic_name = '/test_latch_false'
